71e3bd7d331358e1be81a22b7f214018360a5fbd
Sihao Sun <32914579+sihao-vt@users.noreply.github.com>
2018-03-31 22:18:12 -0400

    Add files via upload

--- MobilityModel/ocean-3d-random-walk.cc
+++ MobilityModel/ocean-3d-random-walk.cc
@@ -19,60 +19,32 @@ Ocean3dRandomWalk::GetTypeId (void)
     .SetParent<MobilityModel> ()
     .SetGroupName ("Mobility")
     .AddConstructor<Ocean3dRandomWalk> ()
-	//This attribute is different from Bounds in random-walk-2d-mobility-model
-	//(0.0 0.0) is position set at time 0;
-	//The range should always be positive
-    .AddAttribute ("Bounds",
-                   "Bounds of the area to cruise.",
-                   RectangleValue (Rectangle (0.0, 1.0, 0.0, 1.0)),
-                   MakeRectangleAccessor (&Ocean3dRandomWalk::m_bounds),
-                   MakeRectangleChecker ())
-    .AddAttribute ("Time",
-                   "Change current direction and speed after moving for this delay.",
-                   TimeValue (Seconds (0.1)),
-                   MakeTimeAccessor (&Ocean3dRandomWalk::m_time),
-                   MakeTimeChecker ())
-    .AddAttribute ("Direction",
-                   "A random variable used to pick the direction (radians).",
-                   StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=6.283184]"),
-                   MakePointerAccessor (&Ocean3dRandomWalk::m_direction),
-                   MakePointerChecker<RandomVariableStream> ())
-    .AddAttribute ("Speed",
-                   "A random variable used to pick the speed (m/s).",
-                   StringValue ("ns3::UniformRandomVariable[Min=1|Max=2]"),
-                   MakePointerAccessor (&Ocean3dRandomWalk::m_speed),
-                   MakePointerChecker<RandomVariableStream> ())
-	.AddAttribute("FileName",
-	              "The position of the file and file name.",
-				  StringValue("src/mobility/model/data.txt"),
-				  MakeStringAccessor(&Ocean3dRandomWalk::m_filename),
-				  MakeStringChecker())
+    .AddAttribute("FileName",
+	          "The position of the file and file name.",
+		StringValue("src/mobility/model/data.txt"),
+		  MakeStringAccessor(&Ocean3dRandomWalk::m_filename),
+		  MakeStringChecker())
     .AddAttribute("MeshSize",
-	              "The meshsize of simulated wave area.",
-				  UintegerValue(1024),
-				  MakeUintegerAccessor (&Ocean3dRandomWalk::m_meshsize),
-				  MakeUintegerChecker<uint64_t> ())			  
+	          "The meshsize of simulated wave area.",
+		  UintegerValue(1024),
+		  MakeUintegerAccessor (&Ocean3dRandomWalk::m_meshsize),
+		  MakeUintegerChecker<uint64_t> ())			  
     .AddAttribute("PatchSize",
-	              "The patchsiize of simulated wave area.",
-				  UintegerValue(2000),
-				  MakeUintegerAccessor (&Ocean3dRandomWalk::m_patchsize),
-				  MakeUintegerChecker<uint64_t> ())
-	.AddAttribute("WindSpeed",
-	              "Simulation windspeed.",
-				  DoubleValue(0),
-				  MakeDoubleAccessor (&Ocean3dRandomWalk::m_windspeed),
-				  MakeDoubleChecker<double> ())
-	.AddAttribute("TimeSteps",
-	              "Time step between two simulated ocean surface.",
-				  DoubleValue(0.02),
-				  MakeDoubleAccessor (&Ocean3dRandomWalk::m_timestep),
-				  MakeDoubleChecker<double> ())
-	.AddAttribute("Steps",
-	              "How many ocean surface instances have been simulated.",
-				  UintegerValue(2000),
-				  MakeUintegerAccessor (&Ocean3dRandomWalk::m_steps),
-				  MakeUintegerChecker<uint64_t> ())
-				  ;
+	        "The patchsiize of simulated wave area.",
+		UintegerValue(2000),
+		MakeUintegerAccessor (&Ocean3dRandomWalk::m_patchsize),
+		MakeUintegerChecker<uint64_t> ())
+    .AddAttribute("TimeSteps",
+	          "Time step between two simulated ocean surface.",
+		  DoubleValue(0.1),
+		  MakeDoubleAccessor (&Ocean3dRandomWalk::m_timestep),
+		  MakeDoubleChecker<double> ())
+    .AddAttribute("Steps",
+	          "How many ocean surface instances have been simulated.",
+		  UintegerValue(600),
+		  MakeUintegerAccessor (&Ocean3dRandomWalk::m_steps),
+		  MakeUintegerChecker<uint64_t> ())
+		  ;
   return tid;
 }
 
@@ -80,77 +52,7 @@ void
 Ocean3dRandomWalk::DoInitialize (void)
 {
   InitPath();
-  DoInitializePrivate ();
-  MobilityModel::DoInitialize ();
-}
-
-void
-Ocean3dRandomWalk::DoInitializePrivate (void)
-{
-  m_helper.Update ();
-  double speed = m_speed->GetValue ();
-  double direction = m_direction->GetValue ();
-  Vector vector (std::cos (direction) * speed,
-                 std::sin (direction) * speed,
-                 0.0);
-  
-  Vector position = m_helper.GetCurrentPosition();
-  position.z=GetHeight(Simulator::Now(),position.x,position.y);
-  m_helper.SetPosition(position);
-  m_helper.SetVelocity (vector);
-  m_helper.Unpause ();
-  
-  Time delayLeft;
-  delayLeft = m_time;
-  DoWalk (delayLeft);
-}
-
-void
-Ocean3dRandomWalk::DoWalk (Time delayLeft)
-{
-  Vector position = m_helper.GetCurrentPosition ();
-  Vector speed = m_helper.GetVelocity ();
-  Vector nextPosition = position;
-  nextPosition.x += speed.x * delayLeft.GetSeconds ();
-  nextPosition.y += speed.y * delayLeft.GetSeconds ();
-  m_event.Cancel ();
-  if (m_bounds.IsInside (nextPosition))
-    {
-      m_event = Simulator::Schedule (delayLeft, &Ocean3dRandomWalk::DoInitializePrivate, this);
-    }
-  else
-    {
-      nextPosition = m_bounds.CalculateIntersection (position, speed);
-      Time delay = Seconds ((nextPosition.x - position.x) / speed.x);
-      m_event = Simulator::Schedule (delay, &Ocean3dRandomWalk::Rebound, this,
-                                     delayLeft - delay);
-    }
- NotifyCourseChange ();
-}
-
-void
-Ocean3dRandomWalk::Rebound (Time delayLeft)
-{
-  m_helper.UpdateWithBounds (m_bounds);
-  Vector position = m_helper.GetCurrentPosition ();
-  Vector speed = m_helper.GetVelocity ();
-  switch (m_bounds.GetClosestSide (position))
-    {
-    case Rectangle::RIGHT:
-    case Rectangle::LEFT:
-      speed.x = -speed.x;
-      break;
-    case Rectangle::TOP:
-    case Rectangle::BOTTOM:
-      speed.y = -speed.y;
-      break;
-    }
-  position.z=GetHeight(Simulator::Now(),position.x,position.y);
-  m_helper.SetPosition(position);
-
-  m_helper.SetVelocity (speed);
-  m_helper.Unpause ();
-  DoWalk (delayLeft);
+  GetHeight();
 }
 
 void
@@ -158,132 +60,117 @@ Ocean3dRandomWalk::DoDispose (void)
 {
   MobilityModel::DoDispose ();
 }
+
 Vector
 Ocean3dRandomWalk::DoGetPosition (void) const
 {
-  m_helper.UpdateWithBounds (m_bounds);
-  return m_helper.GetCurrentPosition ();
+  return m_position;
 }
+
 void
 Ocean3dRandomWalk::DoSetPosition (const Vector &position)
 {
-  //Set the rectangle bounds for this mobility model
-  m_bounds=Rectangle(position.x+m_bounds.xMin,position.x+m_bounds.xMax,position.y+m_bounds.yMin,position.y+m_bounds.yMax);
-  NS_ASSERT_MSG((m_bounds.xMin>=0 && m_bounds.yMin>=0), "The sides of rectangle bounds should always be positive");
-  NS_ASSERT (m_bounds.IsInside (position));
-  m_helper.SetPosition (position);
-  Simulator::Remove (m_event);
-  m_event = Simulator::ScheduleNow (&Ocean3dRandomWalk::DoInitializePrivate, this);
-}
-Vector
-Ocean3dRandomWalk::DoGetVelocity (void) const
-{
-  return m_helper.GetVelocity ();
-}
-int64_t
-Ocean3dRandomWalk::DoAssignStreams (int64_t stream)
-{
-  m_speed->SetStream (stream);
-  m_direction->SetStream (stream + 1);
-  return 2;
+  m_position = position;
+  NotifyCourseChange();
 }
 
-double
-Ocean3dRandomWalk::GetHeight(Time simulation_time, double xlength, double ylength)
+void
+Ocean3dRandomWalk::GetHeight(void)
 {
-    xlength=fmod(xlength,m_patchsize);
-    ylength=fmod(ylength,m_patchsize);
+    Time simulation_time = Simulator::Now();
+    double time=simulation_time.GetSeconds();
+    uint16_t T=time/m_timestep;
+    double result;
 
-	double time=simulation_time.GetSeconds();
-	uint16_t T=time/m_timestep;
-	double result;
-	//std::ifstream infile;
-	//infile.open(m_filename,std::ifstream::in);
+    result =(m_path[T]*((T+1)*m_timestep-time)+m_path[T+1]*(time - T * m_timestep))/m_timestep;
 
-	double interval=(double)m_patchsize/(m_meshsize-1);
-	int16_t XAxis = xlength/interval;
-	int16_t YAxis = ylength/interval;
-	double P1,P2,P3,P4;
-	//int16_t nop=5;//totally 5 parameter at the beginning of the file, we need to skip them.
+    m_position.z=result;
+		//std::cout<<m_position.z<<std::endl;
     
-	//Once we want to get a height in file, we need 4 points around it, because the position is discrete not continuous
-	//And also time is also discrete, so to get a value in any position and time, we totally need 8 points.
-	//uint32_t P11=T*m_meshsize*m_meshsize+YAxis*m_meshsize+XAxis+nop;
-	//uint32_t P12=(T+1)*m_meshsize*m_meshsize+YAxis*m_meshsize+XAxis+nop;
-	//uint32_t P21=T*m_meshsize*m_meshsize+YAxis*m_meshsize+(XAxis+1)+nop;
-	//uint32_t P22=(T+1)*m_meshsize*m_meshsize+YAxis*m_meshsize+(XAxis+1)+nop;
-	//uint32_t P31=T*m_meshsize*m_meshsize+(YAxis+1)*m_meshsize+XAxis+nop;
-	//uint32_t P32=(T+1)*m_meshsize*m_meshsize+(YAxis+1)*m_meshsize+XAxis+nop;
-	//uint32_t P41=T*m_meshsize*m_meshsize+(YAxis+1)*m_meshsize+(XAxis+1)+nop;
-	//uint32_t P42=(T+1)*m_meshsize*m_meshsize+(YAxis+1)*m_meshsize+(XAxis+1)+nop;
-    
-	//double tmp;
-	double v11,v12,v21,v22,v31,v32,v41,v42;
-	v11=path[T][YAxis*m_meshsize+XAxis];
-	v12=path[T+1][YAxis*m_meshsize+XAxis];
-	v21=path[T][YAxis*m_meshsize+XAxis+1];
-	v22=path[T+1][YAxis*m_meshsize+XAxis+1];
-	v31=path[T][(YAxis+1)*m_meshsize+XAxis];
-	v32=path[T+1][(YAxis+1)*m_meshsize+XAxis];
-	v41=path[T][(YAxis+1)*m_meshsize+XAxis+1];
-	v42=path[T+1][(YAxis+1)*m_meshsize+XAxis+1];
+    Time delay = Seconds(m_timestep);
+    Simulator::Schedule(delay, &Ocean3dRandomWalk::GetHeight, this);
 
-	/*for(uint32_t i=0;i<P42+1;i++)
-	{
-      if(i==P11) infile>>v11;
-	  else if(i==P12) infile>>v12;
-	  else if(i==P21) infile>>v21;
-	  else if(i==P22) infile>>v22;
-	  else if(i==P31) infile>>v31;
-	  else if(i==P32) infile>>v32;
-	  else if(i==P41) infile>>v41;
-	  else if(i==P42) infile>>v42;
-	  else infile>>tmp;
-	}*/
-    
-	//interpolation once
-	P1=(v11*(m_timestep*(T+1)-time)+v12*(time-m_timestep*T))/m_timestep;
-	P2=(v21*(m_timestep*(T+1)-time)+v22*(time-m_timestep*T))/m_timestep;
-	P3=(v31*(m_timestep*(T+1)-time)+v32*(time-m_timestep*T))/m_timestep;
-	P4=(v41*(m_timestep*(T+1)-time)+v42*(time-m_timestep*T))/m_timestep;
-    //std::cout<<"Position: "<<P1<<" "<<P2 << " "<<P3<<" "<<P4<<std::endl;
-
-    //interpolation twice
-    result=(P1*((XAxis+1)*interval-xlength)*((YAxis+1)*interval-ylength)+
-	        P2*((XAxis+1)*interval-xlength)*(ylength-YAxis*interval)+
-			P3*(xlength-XAxis*interval)*((YAxis+1)*interval-ylength)+
-			P4*(xlength-XAxis*interval)*((ylength-YAxis*interval)))/(interval*interval);
-    
-	//infile.close();
-
-    return result;
+    NotifyCourseChange();
 }
 
 void
 Ocean3dRandomWalk::InitPath(void)
 {
-    path.resize(m_steps);
-	for(uint16_t i=0;i<m_steps;i++)
-    path[i].resize(m_meshsize*m_meshsize);
+    m_path.resize(m_steps);
+
+    double xlength = m_position.x;
+    double ylength = m_position.y;
+
+    double xaxis=fmod(xlength, m_patchsize);
+    double yaxis=fmod(ylength, m_patchsize);
 
 	std::ifstream infile;
-	double tmp;
 	int16_t nop=5;
 
 	infile.open(m_filename,std::ifstream::in);
 
-	for(uint16_t n=0; n<nop; n++) 
-		infile >> tmp;
-											       
+	double interval = (double) m_patchsize/(m_meshsize-1);
+	uint16_t XAxis = xaxis / interval;
+	uint16_t YAxis = yaxis / interval;
+        //std::cout<<XAxis<<" "<<YAxis<<std::endl;
+        uint32_t p1, p2, p3, p4;
+	
+	double tmp;
+        for(uint16_t i=0;i<nop; i++)
+	{
+            infile>>tmp;
+	}
 
-	for(uint16_t r=0; r<(m_steps); r++)
-	{   
-		for(uint32_t c=0; c<(m_meshsize*m_meshsize); c++)
-		{   
-		    infile >> path[r][c];
-		}   
-	}   
+	p1=YAxis*m_meshsize+XAxis;
+	p2=YAxis*m_meshsize+XAxis+1;
+	p3=(YAxis+1)*m_meshsize+XAxis;
+	p4=(YAxis+1)*m_meshsize+XAxis+1;
+        //std::cout<<p1<<p2<<p3<<p4<<std::endl;
+
+	double h1, h2, h3, h4;
+	for(uint16_t i=0;i<m_steps;i++)
+	{
+	  for(uint32_t j=0; j<m_meshsize*m_meshsize; j++)
+	  {
+	    if(j==p1-1) infile>>h1;
+	    else if(j==p2-1) infile>>h2;
+	    else if(j==p3-1) infile>>h3;
+	    else if(j==p4-1) infile>>h4;
+	    else infile>>tmp;
+	  }
+          double height=
+	          (h1*((XAxis+1)*interval-xaxis)*((YAxis+1)*interval-yaxis)+
+	          h3*((XAxis+1)*interval-xaxis)*(yaxis-YAxis*interval)+
+		  h2*(xaxis-XAxis*interval)*((YAxis+1)*interval-yaxis)+
+		  h4*(xaxis-XAxis*interval)*((yaxis-YAxis*interval)))
+		  /(interval*interval);
+	 // std::cout<<"  "<<height<<std::endl;
+          m_path[i] = height;
+	}
+
 	infile.close();
+}
+
+Ocean3dRandomWalk::Ocean3dRandomWalk() {}
+Ocean3dRandomWalk::~Ocean3dRandomWalk() {}
+
+Vector
+Ocean3dRandomWalk::DoGetVelocity(void) const
+{ return Vector (0.0,0.0,0.0);}
+
+double
+Ocean3dRandomWalk::GetPredictedHeight(Time t) const
+{
+	Time simulation_time=Simulator::Now();
+	double time=simulation_time.GetSeconds();
+	double predict_time=time+t.GetSeconds();
+	double result;
+//	std::cout<<time<< " "<<predict_time<<std::endl; 
+	uint16_t T=predict_time/m_timestep;
+	
+	result =(m_path[T]*((T+1)*m_timestep-time)+m_path[T+1]*(time - T * m_timestep))/m_timestep;
+
+	return result;
 }
 
 
--- MobilityModel/ocean-3d-random-walk.h
+++ MobilityModel/ocean-3d-random-walk.h
@@ -3,11 +3,7 @@
 
 #include "ns3/object.h"
 #include "ns3/nstime.h"
-#include "ns3/event-id.h"
-#include "ns3/rectangle.h"
-#include "ns3/random-variable-stream.h"
 #include "mobility-model.h"
-#include "constant-velocity-helper.h"
 #include <string>
 
 namespace ns3 {
@@ -15,33 +11,26 @@ class Ocean3dRandomWalk : public MobilityModel
 {
 public:
   static TypeId GetTypeId (void);
+  Ocean3dRandomWalk();
+  virtual ~Ocean3dRandomWalk (); 
+  double GetPredictedHeight(Time t) const;
 private:
-  void Rebound (Time timeLeft);
-  void DoWalk (Time timeLeft);
-  void DoInitializePrivate (void);
   virtual void DoDispose (void);
   virtual void DoInitialize (void);
   virtual Vector DoGetPosition (void) const;
   virtual void DoSetPosition (const Vector &position);
-  virtual Vector DoGetVelocity (void) const;
-  virtual int64_t DoAssignStreams (int64_t);
-  
+  virtual Vector DoGetVelocity (void) const; 
   uint64_t m_meshsize;
   uint64_t m_patchsize;
-  double m_windspeed;
   double m_timestep;
   uint64_t m_steps;
   std::string m_filename;
-  std::vector<std::vector<double>> path;
-  double GetHeight(Time simulation_time,double xlength, double ylength);
+  std::vector<double> m_path;
+
+  void GetHeight(void);
   void InitPath(void);
 
-  ConstantVelocityHelper m_helper; //!< helper for this object
-  EventId m_event; //!< stored event ID 
-  Time m_time; //!< Change current direction and speed after this delay
-  Ptr<RandomVariableStream> m_speed; //!< rv for picking speed
-  Ptr<RandomVariableStream> m_direction; //!< rv for picking direction
-  Rectangle m_bounds; //!< Bounds of the area to cruise
+  Vector m_position;
 };
 
 
